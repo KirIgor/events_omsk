ce3aec62a68f285bc5a6ad9228a1fa4a7e4d715c
igor <igor@localhost.localdomain>
2019-06-01 17:02:49 +0600

    init

---
+++ android/app/src/main/kotlin/ru/admomsk/omsk_events/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.admomsk.omsk_events
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
