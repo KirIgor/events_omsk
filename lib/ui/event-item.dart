73245df37e9e2e5abbedea15c6ee2d16fc73f7bb
igor <igor@localhost.localdomain>
2019-07-13 23:32:15 +0600

    bug fix

--- lib/ui/event-item.dart
+++ lib/ui/event-item.dart
@@ -59,19 +59,17 @@ class EventItem extends StatelessWidget {
         currentMillis;
   }
 
-  Widget buildTitle(){
-    if (_event.isBig){
+  Widget buildTitle() {
+    if (_event.isBig) {
       return Text(_event.name,
           style: TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
-              color: Colors.black87);
+              color: Colors.black87));
     }
 
     return Text(_event.name,
-          style: TextStyle(
-              fontSize: 20,
-              color: Colors.black87);
+        style: TextStyle(fontSize: 20, color: Colors.black87));
   }
 
   Widget getBody(BuildContext context) {
@@ -105,7 +103,7 @@ class EventItem extends StatelessWidget {
                                       minRadius: 8,
                                       maxRadius: 8)),
                               Expanded(
-                                  child: buildTitle(),
+                                child: buildTitle(),
                               )
                             ]),
                             Container(
