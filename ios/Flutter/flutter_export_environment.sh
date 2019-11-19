d0a076fbc2f53ae6440d2bb5678eb8e46c2ee2d8
Andrey Arzhannikov <andrey@effective.band>
2019-11-19 23:29:39 +0600

    Prepare to the app publishing to App Store

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/igor/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/igor/Documents/Code/events_omsk"
+export "FLUTTER_ROOT=/Users/andrey_arzhannikov/Workspace/Effective/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/andrey_arzhannikov/Workspace/Effective/Flutter/events_omsk"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/igor/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/andrey_arzhannikov/Workspace/Effective/Flutter/flutter/bin/cache/artifacts/engine/ios-release"
 export "FLUTTER_BUILD_NAME=1.0.4"
-export "FLUTTER_BUILD_NUMBER=6"
+export "FLUTTER_BUILD_NUMBER=7"
